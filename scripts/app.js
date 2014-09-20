9302775cc840f0e8675bd284a972585ea7c11f93
Roman Zaynetdinov <romanznet@gmail.com>
2014-09-20 12:47:24 +0300

    Add possibility to add new point

--- scripts/app.js
+++ scripts/app.js
@@ -83,7 +83,13 @@ L.geoJson(ferries, {
 
 var curClick = null;
 
+var coordField = document.getElementById('coordinates');
+var descField = document.getElementById('description');
+var addBtn = document.getElementById('addBtn');
+
 map.on('click', function (e) {
+
+  // Represent cirle on the map
   if(!curClick) {
     curClick = L.circleMarker(e.latlng, {
       color : '#39C44C'
@@ -91,4 +97,25 @@ map.on('click', function (e) {
   } else {
     curClick.setLatLng(e.latlng);
   }
+
+  coordField.value = e.latlng.lat + ', ' + e.latlng.lng;
 });
+
+// Handle adding new points to the map
+var addedPoints = [];
+
+addBtn.addEventListener('click', function () {
+  var desc = descField.value;
+  if(!desc.length) return;
+
+  var latlng = new L.latLng(coordField
+                              .value
+                              .split(',')
+                              .map(function (el) { return el.trim(); }));
+
+  var point = L.circleMarker(latlng, fishSpotStyle).addTo(map);
+
+  point.bindPopup('<b>Description: </b>' + desc);
+  descField.value = '';
+  addedPoints.push(point);
+}, false);
